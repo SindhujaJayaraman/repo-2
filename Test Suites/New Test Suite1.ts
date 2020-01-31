8bf505a3320242f6b7e085b70746efeb57733b5b
someone <someone@someplace.com>
2020-01-31 13:17:56 +0530

    git commit message

---
+++ Test Suites/New Test Suite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7ffcd711-040d-409e-84d5-fd430c3e4d46</testSuiteGuid>
+   <testCaseLink>
+      <guid>eb49301a-8e52-46e6-a61e-26430c8a1c85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>32788289-7f48-45f9-a15e-c8e1bb9ff6ed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
